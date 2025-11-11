ee461136fccdcdbda9a874b9b7e1d3d8e5d77d5c
HAMILCOR000 <Hamilcor000@warren.k12.in.us>
2025-11-11 12:12:40 -0500

    Day 2

--- day01/snake.py
+++ day01/snake.py
@@ -3,10 +3,11 @@ import pygame, random
 # Initialize pygame
 pygame.init()
 
-# Set display window
+# Set the display window
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+size = (WINDOW_WIDTH, WINDOW_HEIGHT)
+display_surface = pygame.display.set_mode(size)
 pygame.display.set_caption("~~SNEKE~~")
 
 # Set FSP and clock
@@ -14,47 +15,100 @@ FPS = 20
 clock = pygame.time.Clock()
 
 # Set game values
+SNAKE_SIZE = 20
+
+head_x = WINDOW_WIDTH // 2
+head_y = WINDOW_HEIGHT // 2 + 100
+
+snake_dx = 0
+snake_dy = 0
+
+score = 0
 
 # Set colors
-GREEN = (0, 255, 0)
+GREEN = (0, 255, 0)  # (r, g, b)
+DARKGREEN = (10, 50, 10)
+RED = (255, 0, 0)
+DARKRED = (150, 0, 0)
+WHITE = (255, 255, 255)
 
 # Set fonts
+font = pygame.font.SysFont('gabriola', 48)
 
 # Set text
+title_text = font.render("~~Snake~~", True, GREEN, DARKRED)
+title_rect = title_text.get_rect()
+title_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+
+score_text = font.render("Score: 0", True, GREEN, DARKRED)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 10)
+
+game_over_text = font.render("GAMEOVER", True, RED, DARKRED)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+
+continue_text = font.render("Press any key to play again", True, RED, DARKGREEN)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 64)
 
 # Set sounds and music
+pickup_sound = pygame.mixer.Sound("pickup_sound.wav")
 
 # Set images (in this case, use simple rects...so just create their coordinates)
 # For a rectangle you need (top-left x, top-left y, width, height)
+apple_coord = (500, 500, SNAKE_SIZE, SNAKE_SIZE)
+apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
 
-# The main game loop
+head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+head_rect = pygame.draw.rect(display_surface, GREEN, head_coord)
+
 running = True
 while running:
-    # Check to see if the user wants to quit
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        # Move the snake
+     
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                snake_dx = -SNAKE_SIZE
+                snake_dy = 0
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                snake_dx = SNAKE_SIZE
+                snake_dy = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                snake_dx = 0
+                snake_dy = -SNAKE_SIZE
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                snake_dx = 0
+                snake_dy = SNAKE_SIZE
 
     # Add the head coordinate to the first index of the body coordinate list
-    # This will essentilalyl move all of the snakes body by one position in the list
 
-    # Update the x,y position of the snakes head and make a new coordinate
+    head_x += snake_dx
+    head_y += snake_dy
+    head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
 
     # Check for game over
 
-    # Check for collisions
-
     # Update HUD
 
-    # Fill the surface
 
-    # Blit HUD
+    display_surface.fill(DARKGREEN)
 
-    # Blit assets
 
-    # Update display and tick clock
+    display_surface.blit(score_text, score_rect)
+
+    apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
+    head_rect = pygame.draw.rect(display_surface, GREEN, head_coord)
+
+
+    pygame.display.update()
+    clock.tick(20)  
+
 
 # End the game
 pygame.quit()
--- day02/snake_notes.py
+++ day02/snake_notes.py
@@ -40,6 +40,7 @@ title_text = font.render("~~Snake~~", True, GREEN, DARKRED) #make a text object
 title_rect = title_text.get_rect() # gets the box containing the text object
 title_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2) # places the box containing the text object's center to the middle of the screen.
 
+
 #TODO: make a score_text object and assign a font render to it with text "Score: 0", antialias of True, color of GREEN, background color of DARKRED
 #TODO: make a score_rect object by assigning score_text.get_rect() to it.
 #TODO: place the topleft of score_rect to an x coordinate of 10 and y coordinate of 10
@@ -53,7 +54,7 @@ title_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2) # places the box contain
 #TODO: place the center of continue_rect  to an x coordinate of half the WINDOW_WIDTH and y coordinate of half the WINDOW_HEIGHT + 64
 
 # Set sounds and music
-pick_up_sound = pygame.mixer.Sound("pickup_sound.wav")
+pickup_sound = pygame.mixer.Sound("pickup_sound.wav")
 
 # Set images (in this case, use simple rects...so just create their coordinates)
 # For a rectangle you need (top-left x, top-left y, width, height)
